82a4ed26ae557b39e70307232caf97f560a0d460
mhmdbhsk <muhammadbhaska0@gmail.com>
2023-07-02 16:49:36 +0700

    fix: env

--- src/env.mjs
+++ src/env.mjs
@@ -38,8 +38,6 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
