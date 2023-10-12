4f84e6dd3a8cd7a02ebe1d57bacc9df25bcd304b
mhmdbhsk <muhammadbhaska0@gmail.com>
2023-10-12 17:11:44 +0700

    feat: test vtp and update package version

--- src/env.mjs
+++ src/env.mjs
@@ -8,27 +8,27 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    NEXT_PUBLIC_API_URL: z.string().url(),
     NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string().min(1)
-        : z.string().min(1).optional(),
+    process.env.NODE_ENV === "production"
+    ? z.string().min(1)
+    : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string().min(1) : z.string().url()
-    ),
-  },
-
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
-  client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+      ),
+    },
+    
+    /**
+     * Specify your client-side environment variables schema here. This way you can ensure the app
+     * isn't built with invalid env vars. To expose them to the client, prefix them with
+     * `NEXT_PUBLIC_`.
+    */
+   client: {
+     NEXT_PUBLIC_API_URL: z.string().url(),
+     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
   },
 
   /**
